902351c7ca48304ad524a7ba86d9cb6aedba50d3
Екатерина Чмиль <chmil.ekat@yandex.ru>
2025-08-24 11:17:24 +0700

    Add support for relative and absolute paths in Differ.php

--- src/Differ.php
+++ src/Differ.php
@@ -4,12 +4,15 @@ namespace Differ;
 
 function genDiff(string $filepath1, string $filepath2, string $format = 'stylish'): array
 {
-    $content1 = file_get_contents($filepath1);
-    $data1 = json_decode($content1, true);
+    $fullPath1 = realpath($filepath1);
+    $fullPath2 = realpath($filepath2);
 
-    $content2 = file_get_contents($filepath2);
-    $data2 = json_decode($content2, true);
+    if ($fullPath1 === false || $fullPath2 === false) {
+        throw new \Exception("Файл не найден!");
+    }
+
+    $data1 = json_decode(file_get_contents($fullPath1), true);
+    $data2 = json_decode(file_get_contents($fullPath2), true);
 
     return [$data1, $data2];
 }
-
